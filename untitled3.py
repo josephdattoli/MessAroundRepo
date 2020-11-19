67baeec1d9031713554c3ce23eeba0954b52b88a
josephdattoli <joedattoli11@gmail.com>
2020-11-19 09:23:58 -0600

    Commit Of Stuff
    Its a whole bunch of random.

---
+++ untitled3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 09:36:15 2020
+
+@author: joedattoli
+"""
+
+
+import pybaseball as pb
+
+result = pb.playerid_reverse_lookup([15676], key_type='fangraphs')
+
